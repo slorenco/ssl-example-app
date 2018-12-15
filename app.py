8dc87e13899c0c84e5d2d31137d86589fcedf43f
slorenco <ssl@ilight.com>
2018-12-15 13:24:01 -0700

    Initial app boilerplate

--- app.py
+++ app.py
@@ -20,10 +20,12 @@ app.layout = html.Div([
     html.Div(id='display-value')
 ])
 
+
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
